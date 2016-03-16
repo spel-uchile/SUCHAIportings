8917ed6b9d4b638714162ff3d62a0d4c4d85d6ca
Tomas Opazo <tomas.opazo.t@gmail.com>
2016-03-16 13:30:40 -0300

    se sube proyecto en NetBEans para ser la nueva ArchSUCHAI

--- ArchSUCHAI/OS_calls/include/os_queue.h
+++ ArchSUCHAI/OS_calls/include/os_queue.h
@@ -1,6 +1,10 @@
+#ifndef OS_QUEUE_H
+#define OS_QUEUE_H
 
 #if(SCH_TARGET_ARCH == 0)
     #include "../Arch/posix/p_queue.h"
 #elif(SCH_TARGET_ARCH == 1)
     #include "../Arch/freertos/include/p_queue.h"
+#endif
+
 #endif
